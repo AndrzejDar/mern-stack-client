4b92fd67ca141e59a7049715a2c7f61f8e66fd2a
Andrzej Darowski <andrzej.dar@gmail.com>
2022-01-27 15:58:29 +0100

    initial comit of mern stack client

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import "./App.css";
+
+import {Provider} from 'react-redux';
+import store from './store';
+
+import AppNavbar from './components/AppNavbar';
+import ShoppingList from './components/ShoppingList';
+import ItemModal from './components/ItemModal';
+
+
+import {Container} from 'reactstrap';
 
 function App() {
   return (
+    <Provider store={store}>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AppNavbar></AppNavbar>
+      <Container>
+      <ItemModal></ItemModal>
+      <ShoppingList></ShoppingList>
+      </Container>
     </div>
+    </Provider>
   );
 }
 
